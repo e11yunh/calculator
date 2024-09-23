05e2aead4bec95ab913c9d3cb61721c824caab4e
Elliot Yun <e11yunh@gmail.com>
2024-09-23 14:12:14 +0800

    feat: add basic calculator functionality and logic

--- scripts.js
+++ scripts.js
@@ -1,25 +1,34 @@
 function add(n1, n2) {
     return n1 + n2;
-}
+};
 
 function subtract(n1, n2) {
     return n1 - n2;
-}
+};
 
 function multiply(n1, n2) {
     return n1 * n2;
-}
+};
 
 function divide(n1, n2) {
     return n1 / n2;
-}
-
-function operate(operator, n1, n2) {
-    return operator(n1, n2);
 };
 
-function start_calculation(operator, n1, n2) {
-    operate(operator, n1, n2);
+function operate(operator, n1, n2) {
+    switch(operator){
+        case '/':
+            return divide(n1, n2);
+            break;
+        case '*':
+            return multiply(n1, n2);
+            break;
+        case '+':
+            return add(n1, n2);
+            break;
+        case '-':
+            return subtract(n1, n2);
+            break;
+    }
 };
 
 function ready_calculator() {
@@ -33,7 +42,6 @@ function ready_calculator() {
     return final_val;
 };
 
-console.log(ready_calculator())
 
 document.addEventListener('DOMContentLoaded', () => {
     ready_calculator()
@@ -54,11 +62,26 @@ const buttonHeight = ((1 / 5) * 100 - gapSize / (4/3))
 const buttonArr = ["C", "+/-", "%", "/", "7", "8", "9", "*", "4", "5", "6", "-", "1", "2", "3", "+", "0", ".", "="];
 
 for (let i = 0; i < n_buttons - 1; i++) {
-    const buttonElem = document.createElement("div");
+    const buttonElem = document.createElement("button");
     buttonElem.classList.add("button-elem");
     buttonElem.setAttribute('id', `button-${i}`);
     buttonElem.style.height = buttonHeight + "%";
     buttonElem.textContent = buttonArr[i];
+    buttonElem.value = buttonArr[i];
+    buttonElem.addEventListener("click", (event) => {
+        console.log(`Value of Button Event: ${event.target.value} ${typeof event.target.value}`)
+        handle_buttons(event.target.value);
+        // const display_text = document.querySelector("#display-text")
+        // display_text.textContent = `Displaying: ${event.target.value}`
+    });
+    buttonElem.addEventListener("mousedown", (event) => {
+        buttonElem.classList.add("pressed");
+    });
+    buttonElem.addEventListener("mouseup", (event) => {
+        setTimeout(function() {
+            buttonElem.classList.remove("pressed")
+        }, 80)
+    })
 
     if (i === 16) {
         buttonElem.style.width = (2 * buttonWidth) + 1 + "%" ; // to make it proportional to 100%
@@ -68,4 +91,85 @@ for (let i = 0; i < n_buttons - 1; i++) {
     buttonGrid.appendChild(buttonElem);
 };
 
+};
+
+
+// if button is '=', only then shall the operate function be called
+
+// return operate(operator, n1, n2);
+
+
+// update_display should be called whenever a button is input; so always be called
+function update_upper_display(val) {
+    const display_text = document.querySelector("#upper-text");
+    display_text.textContent = val;
+}
+
+
+// if an operator is not called, then the input number is assumed to be n1 and n2 for the case after.
+// So operator can be a flag to distinguish both nums
+
+let n1 = "";
+let n2 = "";
+let operator;
+
+function isButtonsPopulated() {
+    return (typeof n1)
+}
+
+function update_main_display(input) {
+    const display_text = document.querySelector("#display-text")
+    display_text.textContent = input;
+}
+
+function restart() {
+    // let n1 = "";
+    // let n2 = "";
+    // let operator;
+    // update_main_display("0");
+    // update_upper_display("")
+    location.reload();
+}
+
+function handle_buttons(input) {
+    // This function should solely handle the logic surrounding button inputs; 
+    // whether the button should be a n1, operator, or n2
+    const operatorArr = ["/", "*", "-", "+"];
+
+    update_upper_display(input);
+    if (input === "C") {
+        restart();
+    }
+
+    else if (input === "+/-") {
+        if (typeof operator === "undefined") {
+            n1 *= -1;
+            update_main_display(n1);
+        }
+        else if (typeof operator !== "undefined") {
+            n2 *= -1;
+            update_main_display(n2);
+        };
+    }
+
+    else if (operatorArr.includes(input)) {
+        operator = input;
+        n2 = "";
+        update_main_display(input);
+    } 
+
+    else if (input === "=") {
+        let final_val = operate(operator, parseInt(n1), parseInt(n2));
+        n1 = final_val;
+        update_main_display(final_val);
+    }
+    else if (n1 === "" || typeof operator === "undefined") {
+        n1 += input;
+        update_main_display(n1);
+    }
+    else if (n2 === "" || typeof operator !== "undefined") {
+        n2 += input;
+        update_main_display(n2);
+    }
+    console.log(`n1: ${n1}, n2: ${n2}, operator: ${operator}.`)
 };
